ff211866982eba529893ee93ce0f085933e835be
MumenMansour <mumenmansour97@gmail.com>
2022-07-19 21:56:25 -0300

    On branch master
     Initial commit
    
     Changes to be committed:
    	new file:   .flaskenv
    	new file:   .gitignore
    	new file:   app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify
+
+
+# create a flask app
+app = Flask (__name__) 
+
+#criar uma lista 
+lojas = [
+    {
+        'name': 'magazine',
+        'itens':[
+            {
+                'name': 'Jaqueta',
+                'price': 300
+        }
+        ]
+    }
+]
+
+# define the route POST 
+
+#cria uma nova loja
+#post /loja/ data:{name: }
+
+#app cria rota para o post
+@app.route('/loja/', methods=['POST'])
+def create_loja():
+    #return 'create loja'
+    pass
+
+
+@app.route('/loja')
+def get_loja():
+    return jsonify ({'lojas': lojas})
+    
+
